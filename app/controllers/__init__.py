2ebce7595ba3cd206a0753c6f540f957c795fdfa
Matteo Landi <matteo@matteolandi.net>
2014-08-30 18:59:03 +0200

    Add support for limit/offset query string parameters

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -4,6 +4,7 @@
 from collections import namedtuple
 
 import json
+import urllib
 import urllib2
 
 import web
@@ -22,8 +23,12 @@ class Trace(object):
         self.user = User(data['user'])
 
 class IndexController():
+    TRACES_URL = "http://%(host)s/1/traces?limit=%(limit)s&offset=%(offset)s"
     def GET(self):
-        url = "http://%s/1/traces" % web.config.API_HOST
+        data = web.input(limit=1000, offset=0)
+        url = self.TRACES_URL % dict(host=web.config.API_HOST,
+                                     limit=data.limit,
+                                     offset=data.offset)
         data = json.load(urllib2.urlopen(url))
         traces = [Trace(t) for t in data['traces']]
         return web.ctx.render.traces(traces=traces)
