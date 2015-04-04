9e52310544605f40dc4a842ee29c2cabdf0894b7
Matteo Landi <matteo@matteolandi.net>
2015-04-04 11:31:51 -0400

    update weblib and use default intrnalerror handler

--- run_wsgi.py
+++ run_wsgi.py
@@ -1,18 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import web
+from weblib import internalerror
 
 from app import app_factory
-from weblib.logging import create_logger
 
 
 app = app_factory()
-
-
-def nointernalerror():
-    create_logger().exception('Holy shit!')
-    raise web.internalerror('Holy shit!')
-app.internalerror = nointernalerror
+app.internalerror = internalerror('Holy shit!')
 
 app = app.wsgifunc()
